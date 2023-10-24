1563085a7383f2a20106d0489df49b0169d5ffd3
mpmcauliffe <mcauliffe99@gmail.com>
2023-10-23 21:43:59 -0400

    ADD components/MainContainer

---
+++ src/components/MainController/MainController.tsx
@@ -0,0 +1,21 @@
+import { useContext, } from 'react'
+import activeSpeciesContext from '../../context/activeSpeciesContext'
+import { OpSwitch, } from '../opSwitch/OpSwitch'
+
+
+export const MainController = () => {
+    const speciesContext                    = useContext(activeSpeciesContext)
+    const { changeSpeciesOverlay, }         = speciesContext
+
+    const species = ['Black Bear', 'Brown Bear', 'Giant Panda', 'Moon Bear', 'Polar Bear',
+                        'Sloth Bear', 'Spectacled Bear', 'Sun Bear']
+
+    const handleToggle = e => changeSpeciesOverlay(e)
+
+
+    return (
+        <OpSwitch 
+            optButtons={species}
+            handleToggle={handleToggle} />
+    )
+}
